785b88241ca7b5b0a977a17fe07b1d7ee2869b77
Pierre Migeon <pierre@616.local>
2019-05-21 18:26:54 -0700

    added files

---
+++ homozygosity_percent.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl -w
+use strict;
+
+#Description: reads in a tab delinated file of 23andme data and outputs the percent homozygous sites 
+#Usage: perl homozygosity_percent.pl snps.txt
+
+my $snps = $ARGV[0];
+my $equal = 0;
+my $total = 0;
+
+open(IN, '<', $snps);
+while(<IN>)
+{
+	my @line = split(/\t/, $_);
+	if ($_ !~ /^#/)
+	{
+		if (substr($line[3], 0, 1) eq substr($line[3], 1, 1))
+		{
+			$equal++;
+		}
+		$total++;
+	}
+}
+
+print "Of " . $total . " SNPs in the data, " . ($equal / $total) * 100 . "% were homozygous.\n"
